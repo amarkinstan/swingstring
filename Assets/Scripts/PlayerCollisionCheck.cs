d5d6ad9cfa2ee5ce524129e4874b5fbc10b36f09
Amar Kinstan <amar.kinstan@gmail.com>
2015-03-06 21:06:42 +0800

    stuff i forgot

---
+++ Assets/Scripts/PlayerCollisionCheck.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollisionCheck : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+
+    void OnCollisionEnter(Collision who)
+    {
+
+        if (who.collider.isTrigger == false && who.transform.tag != "NoScoreReset")
+        {
+
+            GlobalStuff.TimeSinceLastCollision = 0f;
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/ScoreCalculator.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreCalculator : MonoBehaviour {
+
+    GameObject player;
+    float score;
+    float multi;
+
+    public float factor;
+
+
+	// Use this for initialization
+	void Start () {
+
+        player = GameObject.Find("Player");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (GlobalStuff.Paused == false && GlobalStuff.isDead == false)
+        {
+
+            score = GlobalStuff.Score;
+
+
+            //multiplier is the average speed combined with the time since last block touched
+            //(time in seconds since last block touch)/10f * (Globalstuff.averagePlayerSpeed * factor)
+            multi = Mathf.Clamp(GlobalStuff.TimeSinceLastCollision / 10f, 0f, 1f) * (GlobalStuff.AveragePlayerSpeed * factor);
+
+            GlobalStuff.Multi = multi;
+
+            score += multi * Time.deltaTime * 10f;
+
+            GlobalStuff.Score = score;
+        }
+	
+	}
+}
